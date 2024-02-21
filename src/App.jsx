a3f25c771e1e2cc81296bdaf8e10cb8cec227f7d
kaiumuddin <kaium.uddin2909@gmail.com>
2024-02-21 08:02:10 +0600

    Weather board components separation.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import Header from "./components/header/Header";
+import WeatherBoard from "./components/weather/WeatherBoard";
 
 export default function App() {
     return (
-        <>
+        <div className="bg-body font-[Roboto] text-light bg-[url('./assets/body-bg.png')] bg-no-repeat bg-cover h-screen grid place-items-center">
             <Header />
-        </>
+            <main>
+                <section className="">
+                    <WeatherBoard />
+                </section>
+            </main>
+        </div>
     );
 }
