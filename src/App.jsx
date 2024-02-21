1a94d3b50ad32e0445701a0c1f4d8af51a862682
kaiumuddin <kaium.uddin2909@gmail.com>
2024-02-21 07:38:46 +0600

    Header components separation.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
-function App() {
-    return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import Header from "./components/header/Header";
 
-export default App;
+export default function App() {
+    return (
+        <>
+            <Header />
+        </>
+    );
+}
