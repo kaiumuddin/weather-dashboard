da271e2633388835d02aa2b057db12113323ba28
kaiumuddin <kaium.uddin2909@gmail.com>
2024-02-22 20:33:30 +0600

    Favourite Location toggle.

--- src/components/header/Favourite.jsx
+++ src/components/header/Favourite.jsx
@@ -1,8 +1,11 @@
 import FovouriteIcon from "../../assets/heart.svg";
 
-export default function Favourite() {
+export default function Favourite({ onShow }) {
     return (
-        <div className="p-2 hover:bg-black/30 cursor-pointer flex gap-2 items-center rounded-md transition-all">
+        <div
+            onClick={onShow}
+            className="p-2 hover:bg-black/30 cursor-pointer flex gap-2 items-center rounded-md transition-all"
+        >
             <img src={FovouriteIcon} alt="" />
             <span>Favourite Locations</span>
         </div>
